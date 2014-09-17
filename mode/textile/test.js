8609ce0371865b030a91732dec0bb3c60cec3707
Jon Sangster <jon@ertt.ca>
2014-09-17 02:37:43 -0300

    Add italics, bold, and lists

--- mode/textile/test.js
+++ mode/textile/test.js
@@ -9,11 +9,11 @@
 
 
   FT('simple_header',
-     '[formatting&formatting-header&formatting-header-1&header&header-1 h1.]' +
-     '[header&header-1  Give RedCloth a try!]');
+      '[formatting&formatting-header&formatting-header-1&header&header-1 h1. ]' +
+      '[header&header-1 Give RedCloth a try!]');
 
   MT('plainText',
-     'This is some text.');
+      'This is some text.');
 
   MT('h1',
       '[header&header-1 h1. foo]');
@@ -41,4 +41,60 @@
 
   MT('strong',
       'foo [strong *bar*]');
+
+  MT('italics',
+      'foo [em __bar__]');
+
+  MT('bold',
+      'foo [strong **bar**]');
+
+  MT("ul",
+      "foo",
+      "bar",
+      "",
+      "[variable-2 * foo]",
+      "[variable-2 * bar]");
+
+  MT("ulNoBlank",
+      "foo",
+      "bar",
+      "[variable-2 * foo]",
+      "[variable-2 * bar]");
+
+  MT("ol",
+      "foo",
+      "bar",
+      "",
+      "[variable-2 # foo]",
+      "[variable-2 # bar]");
+
+  MT("olNoBlank",
+      "foo",
+      "bar",
+      "[variable-2 # foo]",
+      "[variable-2 # bar]");
+
+  MT("ulFormatting",
+      "[variable-2 * ][variable-2&em _foo_][variable-2 bar]",
+      "[variable-2 * ][variable-2&strong *][variable-2&em&strong _foo_][variable-2&strong *][variable-2  bar]",
+      "[variable-2 * ][variable-2&strong *foo*][variable-2 bar]");
+
+  MT("olFormatting",
+      "[variable-2 # ][variable-2&em _foo_][variable-2 bar]",
+      "[variable-2 # ][variable-2&strong *][variable-2&em&strong _foo_][variable-2&strong *][variable-2  bar]",
+      "[variable-2 # ][variable-2&strong *foo*][variable-2 bar]");
+
+  MT("ulNested",
+      "[variable-2 # foo]",
+      "[variable-3 ## bar]",
+      "[keyword ### bar]",
+      "[variable-2 #### bar]",
+      "[variable-3 ## bar]");
+
+  MT("olNested",
+      "[variable-2 * foo]",
+      "[variable-3 ** bar]",
+      "[keyword *** bar]",
+      "[variable-2 **** bar]",
+      "[variable-3 ** bar]");
 })();
--- mode/textile/textile.js
+++ mode/textile/textile.js
@@ -12,14 +12,22 @@
 "use strict";
 
 CodeMirror.defineMode("textile", function(cmCfg, modeCfg) {
-
   var header     = 'header'
   ,   formatting = 'formatting'
   ,   em         = 'em'
-  ,   strong     = 'strong';
-
-  var headerRE = /^h([1-6])\./
-  ,   textRE   = /^[^_*]+/;
+  ,   italics    = 'em'
+  ,   strong     = 'strong'
+  ,   bold       = 'strong'
+  ,   list1      = 'variable-2'
+  ,   list2      = 'variable-3'
+  ,   list3      = 'keyword'
+  ;
+  var headerRE    = /^h([1-6])\.\s+/
+  ,   paragraphRE = /^p\.\s+/
+  ,   textRE      = /^[^_*]+/
+  ,   ulRE        = /^(\*+)\s+/
+  ,   olRE        = /^(#+)\s+/
+  ;
 
   if (modeCfg.highlightFormatting === undefined) {
     modeCfg.highlightFormatting = false;
@@ -36,7 +44,9 @@ CodeMirror.defineMode("textile", function(cmCfg, modeCfg) {
   }
 
   function getType(state) {
-    var styles = [];
+    var styles = []
+    ,   listMod
+    ;
 
     if (state.formatting) {
       styles.push(formatting);
@@ -54,20 +64,49 @@ CodeMirror.defineMode("textile", function(cmCfg, modeCfg) {
 
     if (state.header) { styles.push(header); styles.push(header + "-" + state.header); }
     if (state.em) { styles.push(em); }
+    if (state.italics) { styles.push(italics); }
     if (state.strong) { styles.push(strong); }
+    if (state.bold) { styles.push(bold); }
+
+    if (state.list !== false) {
+      var listMod = (state.listDepth - 1) % 3;
+      if (!listMod) {
+        styles.push(list1);
+      } else if (listMod === 1) {
+        styles.push(list2);
+      } else {
+        styles.push(list3);
+      }
+    }
 
     return styles.length ? styles.join(' ') : null;
   }
 
   function blockNormal(stream, state) {
     var ch
-    ,   match;
-
+    ,   match
+    ,   listType
+    ;
     if (stream.eatSpace()) {
       return getType(state);
-    } else if(match = stream.match(headerRE)) {
+    } else if (match = stream.match(headerRE)) {
       state.header = parseInt(match[1]);
       if (modeCfg.highlightFormatting) state.formatting = 'header';
+      return getType(state);
+    } else if (stream.match(ulRE, false) || stream.match(olRE, false)) {
+      if (match = stream.match(ulRE, true)) {
+        listType = 'ul';
+      } else {
+        match = stream.match(olRE, true);
+        listType = 'ol';
+      }
+
+      state.list = true;
+      state.listDepth = match[1].length;
+      state.f = state.inline;
+      if (modeCfg.highlightFormatting) state.formatting = ["list", "list-" + listType];
+      return getType(state);
+    } else if (stream.match(paragraphRE)) {
       return getType(state);
     }
 
@@ -86,26 +125,52 @@ CodeMirror.defineMode("textile", function(cmCfg, modeCfg) {
     ch = stream.next();
 
     if (ch === '_') {
-      if(state.em) { // Remove EM
-        if (modeCfg.highlightFormatting) state.formatting = 'em';
-        t = getType(state);
-        state.em = false;
-        return t;
-      } else { // Add EM
-        state.em = true;
-        if (modeCfg.highlightFormatting) state.formatting = 'em';
-        return getType(state);
+      if (stream.eat('_')) {
+        if(state.italics) { // Remove ITALICS
+          if (modeCfg.highlightFormatting) state.formatting = 'italics';
+          t = getType(state);
+          state.italics = false;
+          return t;
+        } else { // Add ITALICS
+          state.italics = true;
+          if (modeCfg.highlightFormatting) state.formatting = 'italics';
+          return getType(state);
+        }
+      } else {
+        if(state.em) { // Remove EM
+          if (modeCfg.highlightFormatting) state.formatting = 'em';
+          t = getType(state);
+          state.em = false;
+          return t;
+        } else { // Add EM
+          state.em = true;
+          if (modeCfg.highlightFormatting) state.formatting = 'em';
+          return getType(state);
+        }
       }
     } else if (ch === '*') {
-      if(state.strong) { // Remove STRONG
-        if (modeCfg.highlightFormatting) state.formatting = 'strong';
-        t = getType(state);
-        state.strong = false;
-        return t;
-      } else { // Add STRONG
-        state.strong = true;
-        if (modeCfg.highlightFormatting) state.formatting = 'strong';
-        return getType(state);
+      if (stream.eat('*')) {
+        if(state.bold) { // Remove STRONG
+          if (modeCfg.highlightFormatting) state.formatting = 'bold';
+          t = getType(state);
+          state.bold = false;
+          return t;
+        } else { // Add STRONG
+          state.bold = true;
+          if (modeCfg.highlightFormatting) state.formatting = 'bold';
+          return getType(state);
+        }
+      } else {
+        if(state.strong) { // Remove STRONG
+          if (modeCfg.highlightFormatting) state.formatting = 'strong';
+          t = getType(state);
+          state.strong = false;
+          return t;
+        } else { // Add STRONG
+          state.strong = true;
+          if (modeCfg.highlightFormatting) state.formatting = 'strong';
+          return getType(state);
+        }
       }
     }
 
@@ -142,8 +207,12 @@ CodeMirror.defineMode("textile", function(cmCfg, modeCfg) {
         text: handleText,
 
         em: false,
+        italics: false,
         strong: false,
+        bold: false,
         header: false,
+        list: false,
+
         formatting: false
       };
     },
@@ -152,8 +221,11 @@ CodeMirror.defineMode("textile", function(cmCfg, modeCfg) {
 
     blankLine: function(state) {
       state.em = false;
+      state.italics = false;
       state.strong = false;
+      state.bold = false;
       state.header = false;
+      state.list = false;
     }
   };
 });
